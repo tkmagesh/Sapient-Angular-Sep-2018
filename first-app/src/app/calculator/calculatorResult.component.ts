161d206fb7db573e84e5e502fd72bf0b6fab944d
Magesh <tkmagesh77@gmail.com>
2018-09-14 13:02:17 +0530

    introducing calculatorResult component

---
+++ first-app/src/app/calculator/calculatorResult.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+	selector : 'app-calculator-result',
+	templateUrl : 'calculatorResult.component.html',
+	styleUrls : ['calculatorResult.component.css']
+})
+export class CalculatorResultComponent{
+
+	@Input()
+	data : number = 0;
+
+}
