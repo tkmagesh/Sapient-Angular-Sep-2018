dc1776f27e91609cf9738f7fe22efec22e7fa99d
Magesh <tkmagesh77@gmail.com>
2018-09-14 11:44:43 +0530

    introducing CalculatorModel and calculator 2 assignment

---
+++ first-app/src/app/calculator/calculatorOne.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CalculatorModel } from './CalculatorModel';
+
+@Component({
+	selector : 'app-calculator-1',
+	templateUrl : 'calculatorOne.component.html'
+})
+export class CalculatorOneComponent{
+
+	model : CalculatorModel = new CalculatorModel();
+	
+}
